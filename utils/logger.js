a5788551739a60e02532069459caffd570cd39ea
willypelz <pelumiasefon@gmail.com>
2021-06-19 15:36:21 +0100

    [IMP] add logger to the project

---
+++ utils/logger.js
@@ -0,0 +1,12 @@
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+const config = require( '../config/config');
+
+// sentry setup
+Sentry.init({
+    dsn: config.sentryDsn,
+    tracesSampleRate: 1.0,
+  });
+
+
+module.exports = Sentry;
